85b4aee5bdeac842c85a02675a50b611d12f1781
tinhnv <tinhnv@greenglobal.vn>
2023-03-24 09:46:11 +0700

    move WEB-INT folder into webapp, add spring-boot-devtool

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,9 +2,16 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo2Application {
+public class Demo2Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Demo2Application.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
